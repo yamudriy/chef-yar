c25f4400d75861c51a19c2a8d25e5c0e3de76a77
vagrant <vagrant@srv1.(none)>
2012-08-01 12:00:10 +0300

    create working_with_git cookbook

---
+++ cookbooks/working_with_git/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: working_with_git
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+file "/tmp/working_with_git" do
+	content "I'm working with git."
+end
+
+file "/tmp/working_with_git_changes" do
+	mode 0755
+end
