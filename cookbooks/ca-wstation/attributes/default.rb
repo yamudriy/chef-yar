223ebfd32075dff2925da4935fbf6978a928d301
Jarek Nebacry <yamudriy@gmail.com>
2012-10-25 12:23:42 +0300

    CA-WSTATION: package differences between CentOS-5x and CentOS-6x

--- cookbooks/ca-wstation/attributes/default.rb
+++ cookbooks/ca-wstation/attributes/default.rb
@@ -1,14 +1,41 @@
-#
-#
+# CA workstation
+# CAws
 # ca-wstation (caws)
 
-default[:ca-wstation][:needed_packages] = %w{ mc yum-utils pv }
+
+default[:ca-wstation][:needed_packages] = []
+
 
 case platform
+
 when "redhat","centos","scientific"
+  
+  # Some utilities and tools I want to have
+  default[:centos_packages] = %w{ yum-utils mc pv createrepo dstat htop iotop iftop vnstat tree }
+
+  # [CentOS-5.x]
+  # Dev Tools Group (mandatory set):
+  #	yum-utils mc pv autoconf automake binutils bison flex gcc gcc-c++ gettext libtool make pkgconfig redhat-rpm-config rpm-build gdb strace
+  # Mkisofs tool:
+  #	mkisofs
+
+  # [CentOS-6.x]
+  # Dev Tools Group (mandatory set):
+  #	yum-utils mc pv autoconf automake binutils bison flex gcc gcc-c++ gettext libtool make pkgconfig redhat-rpm-config rpm-build patch
+  # Mkisofs tools:
+  #	genisoimage
+  
+  # Development Tools Group (mandatory set)
+  default[:centos_packages] += %w{ autoconf automake binutils bison flex gcc gcc-c++ gettext libtool make pkgconfig redhat-rpm-config rpm-build }
+  
+  # Package differences between CentOS branches, install mkisofs tool and yum-downloadonly plugin also
   if node[:platform_version].to_f >= 6 then
-	set[:ca-wstation][:needed_packages] << "genisoimage"
+	default[:centos_packages] += %w{ patch genisoimage yum-plugin-downloadonly }
   else
-	set[:ca-wstation][:needed_packages] << "mkisofs"
+	default[:centot_packages] += %w{ gdb strace mkisofs yum-downloadonly }
   end
+
+  default[:ca-wstation][:needed_packages] = default[:centos_packages]
+
 end
+
